170d5562ad95675e8be64f7c45501507ef52e162
Eduardonieze <eduardoniez1432@gmail.com>
2025-02-13 13:45:29 +0800

    Updates

--- assets/keyboard2-legacy-3Z9d_3nm.js
+++ assets/keyboard2-legacy-3Z9d_3nm.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-oG89dImz.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
+System.register(["./index-legacy-C4PaA_rL.js"],(function(e,t){"use strict";var i;return{setters:[e=>{i=e.K}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
