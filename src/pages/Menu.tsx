8887d38fb4087b623b3169e9e68e4161bfc049f2
Eduardonieze <eduardoniez1432@gmail.com>
2025-02-13 13:12:38 +0800

    added a code line menu.tsx

--- src/pages/Menu.tsx
+++ src/pages/Menu.tsx
@@ -1,5 +1,6 @@
 import { 
-    IonButtons,
+      IonButton,
+      IonButtons,
       IonContent, 
       IonHeader, 
       IonIcon, 
@@ -13,7 +14,7 @@ import {
       IonTitle, 
       IonToolbar 
     } from '@ionic/react'
-    import {homeOutline, rocketOutline} from 'ionicons/icons';
+    import {homeOutline, logOutOutline, rocketOutline} from 'ionicons/icons';
   import { Redirect, Route } from 'react-router';
   import Home from './Home';
   import About from './About';
@@ -25,14 +26,44 @@ import {
 
     return (
       <IonPage>
-        <IonHeader>
-          <IonToolbar>
-            <IonTitle>Menu</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <IonContent fullscreen>
-        </IonContent>
-      </IonPage>
+      <IonSplitPane contentId="main">
+          <IonMenu contentId="main">
+              <IonHeader>
+                  <IonToolbar>
+                      <IonTitle>
+                          Menu
+                      </IonTitle>
+                  </IonToolbar>
+              </IonHeader>
+              <IonContent>
+                  {path.map((item,index) =>(
+                      <IonMenuToggle key={index}>
+                          <IonItem routerLink={item.url} routerDirection="forward">
+                              <IonIcon icon={item.icon} slot="start"></IonIcon>
+                              {item.name}
+                          </IonItem>
+                      </IonMenuToggle>
+                  ))}
+                  {/*Logout Button*/}
+                  <IonButton routerLink="/it35-lab" routerDirection="back" expand="full">
+                      <IonIcon icon={logOutOutline} slot="start"> </IonIcon>
+                  Logout
+                  </IonButton>
+
+
+
+              </IonContent>
+          </IonMenu>
+          
+          <IonRouterOutlet id="main">
+              <Route exact path="/it35-lab/app/Home" component={Home} />
+              <Route exact path="/it35-lab/app/About" component={About} />
+              <Route exact path="/it35-lab/app">
+                  <Redirect to="/it35-lab/app/Home"/>
+              </Route>
+          </IonRouterOutlet>
+      </IonSplitPane>
+  </IonPage>
     );
   };
   
