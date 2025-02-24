e9873ad703746871b37fbdf01d1d7b0279656e9d
Eduardonieze <eduardonieze1432@gmail.com>
2025-02-24 08:48:38 +0800

    added a code on favorites.tsx

--- src/pages/home-tabs/Favorites.tsx
+++ src/pages/home-tabs/Favorites.tsx
@@ -1,5 +1,10 @@
 import { 
     IonButtons,
+      IonCard,
+      IonCardContent,
+      IonCardHeader,
+      IonCardSubtitle,
+      IonCardTitle,
       IonContent, 
       IonHeader, 
       IonMenuButton, 
@@ -15,7 +20,28 @@ import {
             <IonButtons slot='start'>
               <IonMenuButton></IonMenuButton>
             </IonButtons>
-            <IonTitle>Favorites</IonTitle>
+        () {
+ (
+    <body><IonCard>
+                  <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png"/>
+                  <IonCardHeader>
+                    <IonCardTitle>Card Title</IonCardTitle>
+                    <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                  </IonCardHeader>
+
+                  <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                </IonCard><IonCard>
+                    <img alt="Silhouette of mountains" src="https://ionicframework.com/docs/img/demos/card-media.png" />
+                    <IonCardHeader>
+                      <IonCardTitle>Card Title</IonCardTitle>
+                      <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                    </IonCardHeader>
+
+                    <IonCardContent>Here's a small text description for the card content. Nothing more, nothing less.</IonCardContent>
+                  </IonCard></body>
+  )
+}
+export default Example;
           </IonToolbar>
         </IonHeader>
         <IonContent fullscreen>
