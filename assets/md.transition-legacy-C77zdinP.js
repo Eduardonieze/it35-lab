0efe98114745eb79773f37d88d5d5e8429e90010
Eduardonieze <eduardonieze1432@gmail.com>
2025-03-16 20:26:22 +0800

    Updates

--- assets/md.transition-legacy-C77zdinP.js
+++ assets/md.transition-legacy-C77zdinP.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CHoKMYR9.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.n,i=e.m}],execute:function(){
+System.register(["./index-legacy-BLT0ubpC.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
