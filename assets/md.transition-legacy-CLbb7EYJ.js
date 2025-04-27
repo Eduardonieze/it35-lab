5c0850d536143c469cd27f88ea7ea0cd696f3726
Eduardonieze <eduardonieze1432@gmail.com>
2025-04-28 03:19:45 +0800

    Updates

--- assets/md.transition-legacy-CLbb7EYJ.js
+++ assets/md.transition-legacy-CLbb7EYJ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BbRSlGPA.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-DyZtdb4a.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
