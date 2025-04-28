d2410d0cb5c6090a424ef5294f832e40f9f092d8
Eduardonieze <eduardonieze1432@gmail.com>
2025-04-28 08:20:05 +0800

    Updates

--- assets/ios.transition-legacy-7b5UMhuQ.js
+++ assets/ios.transition-legacy-7b5UMhuQ.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CU2_iY66.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CZw7VTOS.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
